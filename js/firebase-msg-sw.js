ef2e4363e235736639bb144fe4834232bd3bed86
AbilashK <abikspnr@gmail.com>
2017-03-06 00:19:56 +0530

    Implement Push notification with firebase

---
+++ js/firebase-msg-sw.js
@@ -0,0 +1,19 @@
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+
+firebase.initializeApp({
+  'messagingSenderId': '959749382714'
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/images/icons/icon-512x512.png'
+  };
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
